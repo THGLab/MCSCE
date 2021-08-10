5edb5f6c4d0f82e95262087b2fa31a483a1508f6
Jerry <jerry-li1996@berkeley.edu>
2021-08-09 21:29:50 -0700

    Add side chain building code

---
+++ src/mcsce/core/side_chain_builder.py
@@ -0,0 +1,206 @@
+"""
+The main logic for building side chain with Monte Carlo Side Chain Ensemble algorithm, as defined in Bhowmick, Asmit, and Teresa Head-Gordon. "A Monte Carlo method for generating side chain structural ensembles." Structure 23.1 (2015): 44-55.
+
+Coded by Jie Li
+Date created: Jul 29, 2021
+"""
+
+from functools import partial
+import numpy as np
+from numba import njit
+
+# from mcsce import log
+from mcsce.core.rotamer_library import DunbrakRotamerLibrary
+from mcsce.core.build_definitions import sidechain_templates
+from mcsce.libs.libcalc import calc_torsion_angles, place_sidechain_template
+from mcsce.libs.libscbuild import rotate_sidechain
+from tqdm import tqdm
+from copy import deepcopy
+
+# from multiprocessing import set_start_method
+# set_start_method("spawn")
+
+_rotamer_library = DunbrakRotamerLibrary()
+
+@njit
+def choose_random(array):
+    cumulative = [np.sum(array[:i + 1]) for i in range(len(array))]
+    pointer = np.random.random() * cumulative[-1]
+    for idx, num in enumerate(cumulative):
+        if num > pointer:
+            return idx
+
+# def efunc_calculator_proxy(efunc, coord_input):
+#     """
+#     A helper function that takes an efunc object and the inputs, and returns the result from the efunc. 
+#     Used as a proxy of the original efunc to make it picklable and used in multiprocessing
+#     """
+#     return efunc(coord_input)
+
+class efunc_calculator_proxy:
+    def __init__(self, efunc) -> None:
+        self.efunc = efunc
+
+    def __call__(self, coord_input):
+        return self.efunc(coord_input)
+
+def create_side_chain_structure(structure, sidechain_placeholders, energy_calculators, beta, exec_idx):
+    """
+    A function that takes a backbone-only structure and generates a conformation using the Monte Carlo approach
+
+    Parameters
+    ----------
+    structure: Structure object
+        The structure with backbone atoms only
+
+    sidechain_placeholders: list of Structure objects
+        A list of structures with partially grown side chains
+
+    energy_calculators: list of precompiled energy calculation functions
+        A list of functions for calculating the energies of the generated conformations
+
+    beta: float
+        The beta value used for Boltzmann weighting
+
+    Returns
+    ----------
+    all_atom_structure: Structure object
+        The structure including side chain atoms
+
+    succeeded: bool
+        Whether the side chain growth completed successfully
+    """
+    
+    N_CA_C_coords = structure.get_sorted_minimal_backbone_coords()
+    all_backbone_dihedrals = calc_torsion_angles(N_CA_C_coords)
+    all_phi = np.concatenate([[np.nan], all_backbone_dihedrals[2::3]]) * 180 / np.pi
+    all_psi = np.concatenate([all_backbone_dihedrals[::3], [np.nan]]) * 180 / np.pi
+    for idx, resname in enumerate(structure.residue_types):
+        # copy coordinates from the previous growing step to the current placeholder
+        previous_coords = structure.coords
+        structure = deepcopy(sidechain_placeholders[idx])
+        template = sidechain_templates[resname]
+        sidechain_atom_idx = template[1]
+        n_sidechain_atoms = len(sidechain_atom_idx)
+        try:
+            new_coords = structure.coords
+        except:
+            print(structure._data_array)
+        new_coords[:-n_sidechain_atoms] = previous_coords
+        structure.coords = new_coords
+        coords = structure.coords
+        if resname in ["GLY", "ALA"]:
+            # For glycine and alanine, no need for deciding the conformation
+            continue
+        energy_func = energy_calculators[idx]
+        # get all candidate conformations (rotamers) for this side chain
+        candidiate_conformations, candidate_probs = _rotamer_library.retrieve_torsion_and_prob(resname, all_phi[idx], all_psi[idx])
+        # perturb chi angles of the side chains by ~0.5 degrees
+        candidiate_conformations += np.random.normal(scale=0.5, size=candidiate_conformations.shape)
+        residue_bb_coords = N_CA_C_coords[idx * 3: (idx + 1) * 3]
+        energies = []
+        
+        all_coords = np.tile(coords[None], (len(candidiate_conformations), 1, 1))
+        # conformation_collection = []
+        # for each rotamer, decide the resulting conformation and calculate its energy
+        for tor_idx, tors in enumerate(candidiate_conformations):
+            sidechain_with_specific_chi = rotate_sidechain(resname, tors)
+            sc_conformation = place_sidechain_template(residue_bb_coords, sidechain_with_specific_chi[0])
+            # conformation_collection.append(sc_conformation)
+            all_coords[tor_idx, -n_sidechain_atoms:] = sc_conformation[sidechain_atom_idx]
+            # energy = energy_func(coords)
+            # energies.append(energy)
+        energies = energy_func(all_coords)
+        # If all energies are inf, end this growth
+        if np.isinf(energies).all():
+            # log.info("Unresolvable clashes!")
+            # np.save(f"{exec_idx}_{idx}.npy", all_coords)
+            # np.save(f"{exec_idx}_tor.npy", candidiate_conformations)
+            # structure.write_PDB(f"{exec_idx}.pdb")
+            return structure, False
+        # renormalize energies to avoid numerical issues
+        energies -= min(energies)
+        adjusted_weights = candidate_probs * np.exp(-beta * energies)
+        selected_idx = choose_random(adjusted_weights)
+        # set the coordinates of the actual structure that has side chain for this residue grown
+        # coords[-n_sidechain_atoms:] = conformation_collection[selected_idx][sidechain_atom_idx]
+        
+        structure.coords = all_coords[selected_idx]
+    # all side chains have been created, then return the final structure
+    # log.info("Finished without issues!")
+    return structure, True
+
+def create_side_chain_ensemble(structure, n_conformations, efunc_creator, temperature, parallel_worker=16):
+    """
+    Create a given number of conformation ensemble for the backbone-only structure of a protein
+
+    Parameters
+    ----------
+    structure: Structure object
+        The structure with backbone atoms only
+
+    n_conformations: int
+        The total number of conformations to be generated
+
+    efunc_creator: partial function
+        A partial function for creating energy functions for evaluating the generated conformations
+        It should accept atom_label, res_num and res_label as inputs
+
+    temperature: float
+        The temperature value used for Boltzmann weighting
+
+    Returns
+    ----------
+    conformations: list of Structure object
+        The generated conformation structures
+
+    all_success_count: int
+        Total number of the generated structures that are not early-stopped due to unresolvable clashes
+    """
+    # convert temperature to beta
+    beta = 1 / (temperature * 0.008314462) # k unit: kJ/mol/K
+    copied_backbone_structure = deepcopy(structure)
+    sidechain_placeholder_list = []
+    energy_calculator_list = []
+    print("Start preparing energy calculators at different sidechain completion levels")
+    for idx, resname in tqdm(enumerate(structure.residue_types), total=len(structure.residue_types)):
+        template = sidechain_templates[resname]
+        structure.add_side_chain(idx + 1, deepcopy(template))
+        sidechain_placeholder_list.append(deepcopy(structure))
+        if resname not in ["GLY", "ALA"]:
+            energy_func = efunc_creator(structure.atom_labels, 
+                                        structure.res_nums,
+                                        structure.res_labels)
+            # energy_func_proxy = partial(efunc_calculator_proxy, efunc=energy_func)
+            # energy_func_proxy = efunc_calculator_proxy(energy_func)
+            energy_calculator_list.append(energy_func)
+        else:
+            energy_calculator_list.append(None)
+    print("Finished preparing all energy functions. Now start conformation generation")
+    conformations = []
+    all_success_count = 0
+    if parallel_worker == 1:
+        for _ in tqdm(range(n_conformations)):
+            conf, succeeded = create_side_chain_structure(deepcopy(copied_backbone_structure), 
+                            sidechain_placeholder_list, energy_calculator_list, beta)
+            conformations.append(conf)
+            all_success_count += int(succeeded)
+    else:
+        # import multiprocessing
+        # pool = multiprocessing.Pool(processes=parallel_worker)
+
+        import pathos
+        pool = pathos.multiprocessing.ProcessPool(parallel_worker)
+        # results = pool.starmap(create_side_chain_structure, [[deepcopy(copied_backbone_structure), 
+        #                     sidechain_placeholder_list, energy_calculator_list, beta]] * n_conformations)
+        result_iterator = pool.uimap(create_side_chain_structure, [deepcopy(copied_backbone_structure)] * n_conformations, 
+                            [sidechain_placeholder_list] * n_conformations, [energy_calculator_list] * n_conformations,
+                            [beta] * n_conformations, range(n_conformations))
+        conformations = []
+        success_indicator = []
+        all_success_count = 0
+        for result in tqdm(result_iterator, total=n_conformations):
+            conformations.append(result[0])
+            success_indicator.append(result[1])
+    return conformations, success_indicator
+
---
+++ src/mcsce/libs/libscbuild.py
@@ -0,0 +1,309 @@
+"""
+Tools for building side chains with templates and given chi angles
+Some of the functions borrowed from IDP Conformer Generator package (https://github.com/julie-forman-kay-lab/IDPConformerGenerator) developed by Joao M. C. Teixeira
+
+Coded by Jie Li
+Jul 27, 2021
+"""
+
+import numpy as np
+from numba import njit
+from mcsce.libs.libcalc import calc_angle, calc_torsion_angles, rotate_coordinates_Q, hamiltonian_multiplication_Q
+from mcsce.core.build_definitions import sidechain_templates
+
+
+
+# @njit
+# def Q_rotate(coords, rot_vec, angle_rad):
+#     """
+#     Rotate coordinates by radians along an axis.
+
+#     Uses quaternion operations as defined in the IDPConfGen package.
+
+#     Parameters
+#     ----------
+#     coords : nd.array (N, 3), dtype=np.float64
+#         The coordinates to ratote.
+
+#     angle_rad : float
+#         The angle in radians to rotate the coords.
+
+#     rot_vec : (,3)
+#         A 3D space vector around which to rotate coords.
+#         This is assumed to start from origin
+
+#     Returns
+#     -------
+#     nd.array shape (N, 3), dtype=np.float64
+#         The rotated coordinates
+#     """
+#     #assert coords.dim == 2
+#     assert coords.shape[1] == 3
+
+#     b2, b3, b4 = np.sin(angle_rad / 2) * rot_vec
+#     b1 = np.cos(angle_rad / 2)
+
+#     c1, c2, c3, c4 = hamiltonian_multiplication_Q(b1, b2, b3, b4, 0, coords[:, 0], coords[:, 1], coords[:, 2])
+#     d1, d2, d3, d4 = hamiltonian_multiplication_Q(c1, c2, c3, c4, b1, -b2, -b3, -b4)
+#     rotated = np.stack((d2, d3, d4)).T
+
+#     assert rotated.shape[1] == 3
+#     return rotated
+
+# def rotate_to_dihedral_value(coords, ref_coords, target_dihedral):
+#     """
+#     Rotate all atoms defined in coords so that the first atom in coords form a given target dihedral angle value with other three reference points
+
+#     Parameters
+#     ----------
+#     coords : nd.array (N, 3), dtype=np.float64
+#         The coordinates to ratote. The first element of coords is point D
+
+#     ref_coords : nd.array (3, 3), dtype=np.float64
+#         The coordinates of reference points A, B and C
+
+#     target_dihedral : float
+#         The target dihedral angle between A-B-C-D in degrees
+
+#     Returns
+#     -------
+#     nd.array shape (N, 3), dtype=np.float64
+#         The rotated coordinates
+#     """
+#     point_set = np.vstack((ref_coords, coords[0:1]))
+#     original_dihedral = calc_torsion_angles(point_set)[0]
+#     required_rotation = target_dihedral * np.pi / 180 - original_dihedral
+#     shift_arr = ref_coords[1]
+#     shifted_coords = coords - shift_arr
+#     shifted_ref_coords = ref_coords - shift_arr
+#     rotated_shifted_coords = Q_rotate(shifted_coords, shifted_ref_coords[-1], required_rotation)
+#     return rotated_shifted_coords + shift_arr
+
+
+
+def rotate_tor(original_tor, tor, unit_vector, coords, offset):
+    """
+    Rotate a torsion angle from the original value to the target value along a specific axis 
+    (defined by unit_vector) for all atoms in the coords, and apply the offset
+
+    Parameters
+    ----------
+    original_tor : np.float
+        original torsion angle in template.
+    tor : np.float
+        target torsion angle.
+    unit_vector : numpy nd.array, shape (3, ), dtype=float64
+        Unitary vector of the axis to rotate around.
+    coords : numpy nd.array, shape (N, 3), dtype=float64
+        Sidechain coordinates to rotate.
+    offset : numpy nd.array, shape (3, ), dtype=float64
+        Coordinate of the atom at rotation axis.
+
+    Returns
+    -------
+    Rotated coordinates.
+
+    Credit
+    -------
+    Written by Oufan Zhang
+
+    """   
+    displaced = coords - offset
+
+    # rotate
+    rot_angle = original_tor - tor
+    if rot_angle > np.pi:
+            rot_angle -= 2*np.pi
+    elif rot_angle < -np.pi:
+            rot_angle += 2*np.pi
+    rotated = rotate_coordinates_Q(displaced, -unit_vector, rot_angle)
+    
+    return rotated + offset
+
+def rotate_sidechain(res_type, tors):
+    """
+    Function for rotating side chain torsion angles according to privided tors
+
+    Parameters
+    ----------
+    res_type : str 
+        three letter coding of amino acid
+    tors : numpy nd.array, shape (M, ), dtype=float64
+        M depends on res_type, each angle [-180, 180]
+        The chi torsion angles in degrees.
+
+    Returns
+    -------
+    Rotated sidechain coordinates
+
+    Credit
+    -------
+    Original version written by Oufan Zhang, modified by Jie Li
+
+    """
+    assert not res_type in ['ALA', 'GLY']
+
+    # convert degrees to radians
+    tors = tors * np.pi / 180
+
+    # fetch side chain structures and atom labels
+    template_structure, sidechain_idx = sidechain_templates[res_type]
+    sidechain_label = template_structure.data_array[:, 2] # atom name column
+    template = template_structure.coords
+    
+    # chi1 angles in template
+    if res_type == 'SER':
+        N_CA_CB_CG = [np.where(sidechain_label=='N')[0][0], 
+                               np.where(sidechain_label=='CA')[0][0],
+                               np.where(sidechain_label=='CB')[0][0],
+                               np.where(sidechain_label=='OG')[0][0]]
+    elif res_type == 'THR':
+        N_CA_CB_CG = [np.where(sidechain_label=='N')[0][0], 
+                               np.where(sidechain_label=='CA')[0][0],
+                               np.where(sidechain_label=='CB')[0][0],
+                               np.where(sidechain_label=='OG1')[0][0]]
+    elif res_type in ['ILE', 'VAL']:
+        N_CA_CB_CG = [np.where(sidechain_label=='N')[0][0], 
+                               np.where(sidechain_label=='CA')[0][0],
+                               np.where(sidechain_label=='CB')[0][0],
+                               np.where(sidechain_label=='CG1')[0][0]]
+    elif res_type == "CYS":
+        N_CA_CB_CG = [np.where(sidechain_label=='N')[0][0], 
+                               np.where(sidechain_label=='CA')[0][0],
+                               np.where(sidechain_label=='CB')[0][0],
+                               np.where(sidechain_label=='SG')[0][0]]
+    else:
+        N_CA_CB_CG = [np.where(sidechain_label=='N')[0][0], 
+                               np.where(sidechain_label=='CA')[0][0],
+                               np.where(sidechain_label=='CB')[0][0],
+                               np.where(sidechain_label=='CG')[0][0]]
+    ori_chi1 = calc_torsion_angles(template[N_CA_CB_CG, :])[0]  # The original chi1 torsion angle value
+    
+    # chi2 angles in template
+    if res_type == 'MET':       
+        CA_CB_CG_CD = [np.where(sidechain_label=='CA')[0][0],
+                               np.where(sidechain_label=='CB')[0][0],
+                               np.where(sidechain_label=='CG')[0][0],
+                               np.where(sidechain_label=='SD')[0][0]]
+        CB_CG_CD_CE = [np.where(sidechain_label=='CB')[0][0],
+                               np.where(sidechain_label=='CG')[0][0],
+                               np.where(sidechain_label=='SD')[0][0],
+                               np.where(sidechain_label=='CE')[0][0]]
+    elif res_type == 'ILE':
+        CA_CB_CG_CD = [np.where(sidechain_label=='CA')[0][0],
+                               np.where(sidechain_label=='CB')[0][0],
+                               np.where(sidechain_label=='CG1')[0][0],
+                               np.where(sidechain_label=='CD1')[0][0]] 
+    elif res_type in ['HIS', 'HIP', 'HID', 'HIE']:
+        CA_CB_CG_CD = [np.where(sidechain_label=='CA')[0][0],
+                               np.where(sidechain_label=='CB')[0][0],
+                               np.where(sidechain_label=='CG')[0][0],
+                               np.where(sidechain_label=='ND1')[0][0]]
+    elif res_type in [ 'ASN', 'ASP']:
+        CA_CB_CG_CD = [np.where(sidechain_label=='CA')[0][0],
+                               np.where(sidechain_label=='CB')[0][0],
+                               np.where(sidechain_label=='CG')[0][0],
+                               np.where(sidechain_label=='OD1')[0][0]]
+    elif res_type in ['TRP', 'PHE', 'LEU', 'TYR']:
+        CA_CB_CG_CD = [np.where(sidechain_label=='CA')[0][0],
+                               np.where(sidechain_label=='CB')[0][0],
+                               np.where(sidechain_label=='CG')[0][0],
+                               np.where(sidechain_label=='CD1')[0][0]]
+    elif res_type not in ['SER', 'THR', 'VAL', 'CYS']:
+        CA_CB_CG_CD = [np.where(sidechain_label=='CA')[0][0],
+                               np.where(sidechain_label=='CB')[0][0],
+                               np.where(sidechain_label=='CG')[0][0],
+                               np.where(sidechain_label=='CD')[0][0]]
+    
+    # rest of residues
+    if res_type in ['GLN','GLU']:
+        CB_CG_CD_CE = [np.where(sidechain_label=='CB')[0][0],
+                               np.where(sidechain_label=='CG')[0][0],
+                               np.where(sidechain_label=='CD')[0][0],
+                               np.where(sidechain_label=='OE1')[0][0]]
+    elif res_type == 'LYS':
+        CB_CG_CD_CE = [np.where(sidechain_label=='CB')[0][0],
+                               np.where(sidechain_label=='CG')[0][0],
+                               np.where(sidechain_label=='CD')[0][0],
+                               np.where(sidechain_label=='CE')[0][0]]
+        CG_CD_CE_CZ = [np.where(sidechain_label=='CG')[0][0],
+                               np.where(sidechain_label=='CD')[0][0],
+                               np.where(sidechain_label=='CE')[0][0],
+                               np.where(sidechain_label=='NZ')[0][0]]
+    elif res_type == 'ARG':
+        CB_CG_CD_CE = [np.where(sidechain_label=='CB')[0][0],
+                               np.where(sidechain_label=='CG')[0][0],
+                               np.where(sidechain_label=='CD')[0][0],
+                               np.where(sidechain_label=='NE')[0][0]]
+        CG_CD_CE_CZ = [np.where(sidechain_label=='CG')[0][0],
+                               np.where(sidechain_label=='CD')[0][0],
+                               np.where(sidechain_label=='NE')[0][0],
+                               np.where(sidechain_label=='CZ')[0][0]]
+        CD_NE_CZ_NH1 = [np.where(sidechain_label=='CD')[0][0],
+                               np.where(sidechain_label=='NE')[0][0],
+                               np.where(sidechain_label=='CZ')[0][0],
+                               np.where(sidechain_label=='NH1')[0][0]]
+        ori_chi5 = calc_torsion_angles(template[CD_NE_CZ_NH1, :])[0]
+        
+    
+    # place CB at origin for rotation, exclude bb atoms and HA
+    CB = template[np.where(sidechain_label=='CB')[0][0]]
+    unit_vector = CB/np.linalg.norm(CB)
+    # HA = np.where(sidechain_label=='HA')[0][0]
+    # chi1_idx = np.delete(sidechain_idx, np.where(sidechain_idx==HA)[0][0])
+    chi1_idx = sidechain_idx
+    template[chi1_idx, :] = rotate_tor(ori_chi1, tors[0], unit_vector, 
+                                             template[chi1_idx, :], CB)
+    if len(tors) == 1:
+        return template, sidechain_idx
+    
+    ori_chi2 = calc_torsion_angles(template[CA_CB_CG_CD, :])[0]
+    CGs = template[5]
+    assert sidechain_label[5] in ['CG','CG1']
+    unit_vector = (CGs - CB)/np.linalg.norm(CGs - CB)
+    HBs = np.where(np.isin(sidechain_label, ['HB','HB2','HB3','CB','CG2','HG21','HG22','HG23']))[0]
+    chi2_idx = np.delete(chi1_idx, np.where(np.isin(chi1_idx, HBs))[0])
+    template[chi2_idx, :] = rotate_tor(ori_chi2, tors[1], unit_vector, 
+                                             template[chi2_idx, :], CGs)
+    if len(tors) == 2:
+        return template, sidechain_idx
+    
+    ori_chi3 = calc_torsion_angles(template[CB_CG_CD_CE, :])[0]
+    CDs = template[6]
+    assert sidechain_label[6] in ['CD','SD']
+    unit_vector = (CDs - CGs)/np.linalg.norm(CDs - CGs)
+    HGs = np.where(np.isin(sidechain_label, ['HG','HG2','HG3','CG','CG1']))[0]
+    chi3_idx = np.delete(chi2_idx, np.where(np.isin(chi2_idx, HGs))[0])
+    template[chi3_idx, :] = rotate_tor(ori_chi3, tors[2], unit_vector, 
+                                             template[chi3_idx, :], CDs)
+    if len(tors) == 3:
+        #print('rotated chi3:', np.degrees(calc_torsion_angles(template[CB_CG_CD_CE, :])[0]))
+        return template, sidechain_idx
+    
+    ori_chi4 = calc_torsion_angles(template[CG_CD_CE_CZ, :])[0]
+    CEs = template[7]
+    assert sidechain_label[7] in ['NE','CE']
+    unit_vector = (CEs - CDs)/np.linalg.norm(CEs - CDs)
+    HDs = np.where(np.isin(sidechain_label, ['HD','HD2','HD3','CD','SD']))[0]
+    chi4_idx = np.delete(chi3_idx, np.where(np.isin(chi3_idx, HDs))[0])
+    template[chi4_idx, :] = rotate_tor(ori_chi4, tors[3], unit_vector, 
+                                             template[chi4_idx, :], CEs)
+    if len(tors) == 4:
+        return template, sidechain_idx
+    
+    CZ = template[8]
+    unit_vector = (CZ - CEs)/np.linalg.norm(CZ - CEs)
+    assert sidechain_label[19] == 'HE'
+    chi5_idx = np.delete(chi4_idx, np.where(np.isin(chi4_idx, [19, 7]))[0])
+    template[chi5_idx, :] = rotate_tor(ori_chi5, tors[4], unit_vector, 
+                                             template[chi5_idx, :], CZ)
+    
+    return template, sidechain_idx
+
+
+if __name__ == "__main__":
+    #ARG
+    template_coord, _ = rotate_sidechain("GLN", np.array([123, 35, -162]))
+    arg_temp = sidechain_templates["GLN"][0]
+    arg_temp.coords = template_coord
+    arg_temp.write_PDB("rotated_GLN.pdb")
